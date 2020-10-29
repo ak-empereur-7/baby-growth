9bf64e969edcfd71bb3e1dcaf246162980023384
AKIHISA SHIMADA <empereur@empereur-2.local>
2020-10-29 23:36:35 +0900

    feat: create header(#12)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import '@fortawesome/fontawesome-free/js/all'
 import 'bootstrap'
 import '../stylesheets/application'
+import '../stylesheets/base'
